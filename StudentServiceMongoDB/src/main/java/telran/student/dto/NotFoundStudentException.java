c21926b9d4c65770774bb505d7bd8223b2cbc275
Elena <Elena@DESKTOP-G18ORRF>
2018-10-05 19:37:44 +0300

    asssjhjhjh

---
+++ StudentServiceMongoDB/src/main/java/telran/student/dto/NotFoundStudentException.java
@@ -0,0 +1,23 @@
+package telran.student.dto;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+public class NotFoundStudentException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public NotFoundStudentException() {
+		super();
+		//TODO
+	}
+	public NotFoundStudentException(String message) {
+		super(message);
+		//TODO
+	}
+
+}
---
+++ StudentServiceMongoDB/src/main/java/telran/student/dto/ScoreDto.java
@@ -0,0 +1,17 @@
+package telran.student.dto;
+
+public class ScoreDto {
+	
+	String examName;
+	int score;
+	
+	public String getExamName() {
+		return examName;
+	}
+	public int getScore() {
+		return score;
+	}
+	
+	
+
+}
